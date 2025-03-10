9a443a8ab6e778064af444f2312c1aed34c23cd0
JamesSewter <jamessewter@gmail.com>
2025-03-10 17:20:43 +0000

    register-login-forms

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import api from '../api';
 import { useNavigate } from 'react-router-dom';
 import { ACCESS_TOKEN, REFRESH_TOKEN } from '../constants';
+import '../styles/form.css';
 
 function Form({ route, method }) {
   const [username, setUsername] = useState('');
@@ -11,22 +12,21 @@ function Form({ route, method }) {
 
   const name = method === 'login' ? 'Login' : 'Register';
 
-  const handleSubmit = async (event) => {
+  const handleSubmit = async (e) => {
     setLoading(true);
-    event.preventDefault();
+    e.preventDefault();
 
     try {
       const res = await api.post(route, { username, password });
-      if (method === "login") {
-        localStorage.setItem(ACCESS_TOKEN, res.data.access)
-        localStorage.setItem(REFRESH_TOKEN, res.data.refresh)
-        navigate("/")
+      if (method === 'login') {
+        localStorage.setItem(ACCESS_TOKEN, res.data.access);
+        localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
+        navigate('/');
+      } else {
+        navigate('/login');
       }
-      else {
-        navigate("/login")
-      }
-    } catch (err) {
-      alert(err);
+    } catch (error) {
+      alert(error);
     } finally {
       setLoading(false);
     }
@@ -39,15 +39,15 @@ function Form({ route, method }) {
         className='form-input'
         type='text'
         value={username}
-        onChange={(event) => setUsername(event.target.value)}
+        onChange={(e) => setUsername(e.target.value)}
         placeholder='Username'
       />
       <input
         className='form-input'
         type='password'
         value={password}
-        onChange={(event) => setPassword(event.target.value)}
-        placeholder='password'
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder='Password'
       />
       <button className='form-button' type='submit'>
         {name}
@@ -55,3 +55,5 @@ function Form({ route, method }) {
     </form>
   );
 }
+
+export default Form;
